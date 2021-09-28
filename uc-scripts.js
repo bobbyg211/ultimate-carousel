31145bdb40d8565f63eb9ce953975aa8b59e25b1
bobbyg211 <robertdg96@gmail.com>
2021-09-28 16:37:06 -0400

    Initial commit

---
+++ uc-scripts.js
@@ -0,0 +1,19 @@
+// const UC = (element, options) => {
+//   const self = {
+//     element: document.querySelector(element),
+//     print: () => {
+//       console.log(self.element);
+//     },
+
+//     activate: () => {
+//       self.element.classList.add("uc-slider");
+//       self.element.children.classList.add("uc-slide");
+//     },
+//   };
+
+//   return self;
+// };
+
+// const carousel1 = UC(".slider-wrapper", { infinite: true });
+
+// carousel1.activate();
